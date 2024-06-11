c57f4c20de5b50bf9ab61c125282889e661b1e0a
alfre4500 <alfreagor@gmail.com>
2024-06-11 19:00:38 -0300

    barra de busqueda

--- index.js
+++ index.js
@@ -19,3 +19,21 @@ document.addEventListener("DOMContentLoaded", function() {
         tarjetas.style.transform = `translateX(-${indice * tarjetaWidth}px)`;
     }
 });
+//barra de busqueda//
+document.addEventListener("DOMContentLoaded", function() {
+    const inputBusqueda = document.getElementById("busqueda");
+    const lista = document.getElementById("lista").getElementsByTagName("li");
+
+    inputBusqueda.addEventListener("keyup", function() {
+        const filtro = inputBusqueda.value.toLowerCase();
+
+        Array.from(lista).forEach(function(item) {
+            const texto = item.textContent.toLowerCase();
+            if (texto.includes(filtro)) {
+                item.style.display = "block";
+            } else {
+                item.style.display = "none";
+            }
+        });
+    });
+});
