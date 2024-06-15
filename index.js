ddb65ddff7cb9d3ff325e45041c57007e96d28e9
alfre4500 <alfreagor@gmail.com>
2024-06-15 19:22:20 -0300

    destinos

--- index.js
+++ index.js
@@ -11,28 +11,6 @@ function updateTime() {
 
 setInterval(updateTime, 1000);
 
-//carrusel//
-document.addEventListener("DOMContentLoaded", function() {
-    const anteriorBtn = document.getElementById("anterior");
-    const siguienteBtn = document.getElementById("siguiente");
-    const tarjetas = document.querySelector(".tarjetas");
-    let indice = 0;
-
-    anteriorBtn.addEventListener("click", function() {
-        indice = Math.max(indice - 1, 0);
-        actualizarCarrusel();
-    });
-
-    siguienteBtn.addEventListener("click", function() {
-        indice = Math.min(indice + 1, tarjetas.children.length - 1);
-        actualizarCarrusel();
-    });
-
-    function actualizarCarrusel() {
-        const tarjetaWidth = tarjetas.firstElementChild.offsetWidth;
-        tarjetas.style.transform = `translateX(-${indice * tarjetaWidth}px)`;
-    }
-});
 //barra de busqueda//
 document.addEventListener("DOMContentLoaded", function() {
     const inputBusqueda = document.getElementById("busqueda");
