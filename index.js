56471f1414f3bd751d4ad45e3c41ccf28d1ca195
alfre4500 <alfreagor@gmail.com>
2024-06-18 15:58:35 -0300

    cambios

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 //reloj//
 function updateTime() {
-    const now = new Date();
-    const hours = now.getHours().toString().padStart(2, '0');
-    const minutes = now.getMinutes().toString().padStart(2, '0');
-    const seconds = now.getSeconds().toString().padStart(2, '0');
+    let now = new Date();
+    let hours = now.getHours().toString().padStart(2, '0');
+    let minutes = now.getMinutes().toString().padStart(2, '0');
+    let seconds = now.getSeconds().toString().padStart(2, '0');
 
-    const timeString = `${hours}:${minutes}:${seconds}`;
+    let timeString = `${hours}:${minutes}:${seconds}`;
     document.getElementById('time').textContent = timeString;
 }
 
